a6c61885dcda151aab86e8175a6b2a7218cb74f9
Chris Tacke <ctacke@gmail.com>
2026-07-08 21:49:46 -0500

    fix: build issues

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = "Stop"
 
 $binDir = "Source/PiDbg.VsCodeExtension/bin"
+New-Item -ItemType Directory -Force -Path $binDir | Out-Null
 Get-ChildItem $binDir -Force | Where-Object { $_.Name -ne ".gitkeep" } | Remove-Item -Recurse -Force
 
 dotnet publish Source/PiDbg.DebugAdapter/PiDbg.DebugAdapter.csproj -c Release -r win-x64 --self-contained true -o $binDir
